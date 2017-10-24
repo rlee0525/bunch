00660f4d072455aabfc701329518b78f25f5e305
Raymond Lee <raylee0525@gmail.com>
2017-10-24 13:55:29 -0700

    react navigation with redux integration

--- App.js
+++ App.js
@@ -1,34 +1,12 @@
 import React from 'react';
 import { Font } from 'expo';
-import { StyleSheet, Text, View, Navigator } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { StyleSheet, Text, View, 
+         Navigator, AppRegistry } from 'react-native';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      fontLoaded: false
-    };
-  }
-
-  async componentDidMount() {
-    await Font.loadAsync({
-      'agb': require('./assets/fonts/agb.ttf'),
-    });
-
-    this.setState({ fontLoaded: true });
-  }
-
-  render() {
-    if (!this.state.fontLoaded) return null;
-    
-    return (
-      <View style={styles.container}>
-        <Text style={styles.whiteFont}>BUNCH</Text>
-      </View>
-    );
-  }
-}
+import AppReducer from './src/reducers';
+import AppWithNavigationState from './src/navigators/AppNavigator';
 
 const styles = StyleSheet.create({
   container: {
@@ -43,3 +21,57 @@ const styles = StyleSheet.create({
     fontSize: 30
   }
 });
+
+class App extends React.Component {
+  store = createStore(AppReducer);
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fontLoaded: false,
+      loading: true
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'agb': require('./assets/fonts/agb.ttf'),
+    });
+
+    this.setState({ fontLoaded: true });
+
+    await this.resolveAfter2Seconds(20);
+    this.setState({ loading: false });
+  }
+
+  resolveAfter2Seconds(x) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(x);
+      }, 2000);
+    });
+  }
+
+  render() {
+    if (!this.state.fontLoaded) return null;
+
+    if (this.state.loading) {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.whiteFont}>BUNCH</Text>
+        </View>
+      );
+    }
+    
+    return (
+      <Provider store={this.store}>
+        <AppWithNavigationState />
+      </Provider>
+    );
+  }
+}
+
+AppRegistry.registerComponent('ReduxExample', () => ReduxExampleApp);
+
+export default App;
